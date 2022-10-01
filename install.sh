910ceb700590b654429d17cd361d875a7871679b
Pfau, Sascha <sascha_a.pfau@nordlb.de>
2022-10-01 09:43:48 +0000

    move tool installation into gitpod.yml

--- install.sh
+++ install.sh
@@ -3,9 +3,6 @@
 # setup k3d
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-# create demo cluster
-k3d cluster create demo
-
 # install latest kubectl
 sudo curl -L "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl
 sudo chmod +x /usr/local/bin/kubectl
